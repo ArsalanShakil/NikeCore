6b780cf231e5a2df4eb58672c230b012ca6a920c
DARSHIL SHAH <my.name@example.com>
2021-09-23 13:42:28 +0300

    Added Working Onboarding Screen with only first time click

--- app/src/main/java/com/example/nikecore/SplashScreen.kt
+++ app/src/main/java/com/example/nikecore/SplashScreen.kt
@@ -3,18 +3,16 @@ package com.example.nikecore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
-
-
+import com.example.nikecore.ui.onBoarding.OnBoardingActivity
 
 
 class SplashScreen : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this@SplashScreen, MainActivity::class.java))
-
+        startActivity(Intent(this@SplashScreen,
+            OnBoardingActivity::class.java))
         finish()
 
     }
