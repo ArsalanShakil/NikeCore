41f04a11fd4ed8057dab7c551d22a9537044ec47
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-09-30 22:18:59 +0300

    add bar chart into stats fragment

--- app/src/main/java/com/example/nikecore/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/nikecore/statistics/StatisticsFragment.kt
@@ -1,14 +1,21 @@
 package com.example.nikecore.statistics
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.nikecore.R
+import com.example.nikecore.others.CustomMarkerView
 import com.example.nikecore.others.TrackingUtilities
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.statistics_fragment.*
 import java.lang.Math.round
@@ -60,6 +67,43 @@ class StatisticsFragment : Fragment() {
                 caloriesValueStatsTxt.text = totalCalories
             }
         })
+        statisticsViewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val allAvgSpeeds = it.indices.map { i -> BarEntry(i.toFloat(), it[i].avgSpeedInKMH) }
+                val bardataSet = BarDataSet(allAvgSpeeds, "Avg Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(requireContext(), R.color.yellow)
+                }
+                barChart.data = BarData(bardataSet)
+                barChart.marker = CustomMarkerView(it.reversed(), requireContext(), R.layout.marker_view)
+                barChart.invalidate()
+            }
+        })
     }
+
+    private fun setupBarChart() {
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        barChart.axisLeft.apply {
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        barChart.axisRight.apply {
+            axisLineColor = Color.BLACK
+            textColor = Color.BLACK
+            setDrawGridLines(false)
+        }
+        barChart.apply {
+            description.text = "Avg Speed Over Time"
+            legend.isEnabled = false
+        }
+    }
+
 
 }
