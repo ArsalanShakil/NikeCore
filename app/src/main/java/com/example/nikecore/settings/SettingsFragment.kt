092e12dd61c7f65f8beaf2028f26dee76a226f40
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-10-02 22:40:59 +0300

    bluetooth heart rate monitoring working

--- app/src/main/java/com/example/nikecore/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/nikecore/settings/SettingsFragment.kt
@@ -1,27 +1,18 @@
 package com.example.nikecore.settings
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.nikecore.R
-import com.example.nikecore.others.Constants.KEY_NAME
-import com.example.nikecore.others.Constants.KEY_WEIGHT
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.settings_fragment.*
-import www.sanju.motiontoast.MotionToast
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
-    @Inject
-    lateinit var sharedPreferences: SharedPreferences
     private lateinit var viewModel: SettingsViewModel
 
     override fun onCreateView(
@@ -33,50 +24,16 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loadFieldsFromSharedPref()
-        saveChangesBtn.setOnClickListener {
-            val success = applyChangesToSharedPref()
-            if (success) {
-                MotionToast.darkToast(requireActivity(),
-                    getString(R.string.info),
-                    getString(R.string.saved_changes),
-                    MotionToast.TOAST_SUCCESS,
-                    MotionToast.GRAVITY_BOTTOM,
-                    MotionToast.SHORT_DURATION,
-                    ResourcesCompat.getFont(requireContext(),R.font.helvetica_regular))
-                findNavController().navigate(R.id.action_settingsFragment_to_navigation_run)
-            } else {
-                MotionToast.darkToast(requireActivity(),
-                    getString(R.string.info),
-                    getString(R.string.please_enter_all),
-                    MotionToast.TOAST_ERROR,
-                    MotionToast.GRAVITY_BOTTOM,
-                    MotionToast.SHORT_DURATION,
-                    ResourcesCompat.getFont(requireContext(),R.font.helvetica_regular))
-            }
+
+        editInfoBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment_to_editInfoFragment)
         }
-    }
-
-    private fun loadFieldsFromSharedPref() {
-        val name = sharedPreferences.getString(KEY_NAME, "")
-        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
-        editTextPersonNameSettings.setText(name)
-        editTextWeightSettings.setText(weight.toString())
-    }
-
-    private fun applyChangesToSharedPref(): Boolean {
-        val nameText = editTextPersonNameSettings.text.toString()
-        val weightText = editTextWeightSettings.text.toString()
-        val heightText = editTextHeightSettings.text.toString()
-
-        if (nameText.isEmpty() || weightText.isEmpty() || heightText.isEmpty()) {
-            return false
+        heartRateBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_settingsFragment_to_heartRateFragment)
         }
-        sharedPreferences.edit()
-            .putString(KEY_NAME, nameText)
-            .putFloat(KEY_WEIGHT, weightText.toFloat())
-            .apply()
-        return true
+
+
     }
+
 
 }
---
+++ app/src/main/java/com/example/nikecore/settings/editinfo/EditInfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.nikecore.settings.editinfo
+
+import androidx.lifecycle.ViewModel
+
+class EditInfoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
